6f863346112d0dbebcd5a84eae7f4f5ded0a57c9
Darren <darrenpiersekelly@gmail.com>
2025-08-16 09:23:14 +1000

    Some build fixes to use compatible firebase libs

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true,
+  // By default, babel-loader ignores files in node_modules.
+  // Firebase packages are pre-compiled and including them in transpileDependencies
+  // can lead to build errors by interfering with webpack's module resolution.
+  transpileDependencies: [],
 });
